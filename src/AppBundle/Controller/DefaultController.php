bcabd0ccb26934572609defdf7e7a6f65e589b40
Grec Maxime <maxime.grec@gmail.com>
2017-05-17 17:42:13 +0200

    add find all en find by id

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -34,9 +34,7 @@ class DefaultController extends Controller
         $userRepo = $this->get('doctrine')->getManager()->getRepository('AppBundle:User');
         $user = $userRepo->findOneById($id);
         
-        if (!$user){
-            throw new NotFoundHttpException();    
-        }
+       
         
         // render view
         return $this->render('AppBundle:Default:user-single.html.twig', compact('user'));
