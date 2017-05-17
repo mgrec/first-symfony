b5876d9d9ada2eac09c4c3b5b46c8c4796198a39
Grec Maxime <maxime.grec@gmail.com>
2017-05-17 17:42:03 +0200

    add find all en find by id

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -3,6 +3,7 @@
 namespace AppBundle\Controller;
 
 use AppBundle\AppBundle;
+use AppBundle\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
@@ -17,16 +18,28 @@ class DefaultController extends Controller
 
     public function usersAction()
     {
-        return $this->render('AppBundle:Default:users.html.twig');
+        // execute request
+        $userRepo = $this->get('doctrine')->getManager()->getRepository('AppBundle:User');
+        $users = $userRepo->findAll();
+        
+        return $this->render('AppBundle:Default:users.html.twig', compact('users'));
     }
 
     public function userSingleAction(Request $request)
     {
         // get param: id user
-        $request->get('id');
+        $id = $request->get('id');
+
+        // execute request
+        $userRepo = $this->get('doctrine')->getManager()->getRepository('AppBundle:User');
+        $user = $userRepo->findOneById($id);
+        
+        if (!$user){
+            throw new NotFoundHttpException();    
+        }
         
         // render view
-        return $this->render('AppBundle:Default:user-single.html.twig');
+        return $this->render('AppBundle:Default:user-single.html.twig', compact('user'));
     }
 
     public function userDeleteAction(Request $request)
